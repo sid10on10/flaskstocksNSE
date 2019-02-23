da0f3d910d0dd8dfdcb4d9a48329a4c94595146a
Siddhant Singh <sid10on10@gmail.com>
2019-02-23 15:20:23 +0530

    two plots

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
 # import flask
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, Response
 import quandl
 import pandas as pd
+import matplotlib  
+matplotlib.use('TkAgg')   
+import matplotlib.pyplot as plt, mpld3
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from io import BytesIO
+
 
 # # app Flask
 # -----------------------------------------------------|
 app = Flask(__name__)
+quandl.ApiConfig.api_key = "VKXmKjxxSpX-ydXye9mu"
 
 
 @app.route('/',methods=['GET','POST'])
@@ -19,8 +27,18 @@ def index():
 		end_date = request.form['end_date']
 		data = quandl.get("NSE/{}".format(ticker), start_date=start_date, end_date=end_date)
 		df = pd.DataFrame(data)
+		#image = df['Close'].plot(legend=True,figsize=(10,4))
+		Y = df['Close']
+		plot_1 = plt.plot(Y)
+		# Let's go ahead and plot out several moving averages
+		plot_2 = Y.rolling(window=20, center=False).mean().plot(legend=True,figsize=(10,4))
+		text= mpld3.fig_to_html(plot_1)
+		text2 = mpld3.fig_to_html(plot_2)
 
-		return render_template('simple.html',  tables=[df.to_html(classes='data', header="true")])
+	
+
+
+		return render_template('simple.html',  tables=[df.to_html(classes='data', header="true")], html=text, html2=text2)
 
 
 
